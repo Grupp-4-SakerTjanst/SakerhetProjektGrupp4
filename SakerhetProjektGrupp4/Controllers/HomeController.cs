0d7c8ed0a2560b4c78b9c3a1f41288b30bbc81be
Minna Blomqvist <mibl0003@student.hv.se>
2020-05-18 12:15:52 +0200

    Kopplat till REST

--- SakerhetProjektGrupp4/Controllers/HomeController.cs
+++ SakerhetProjektGrupp4/Controllers/HomeController.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SakerhetProjektGrupp4.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace SakerhetProjektGrupp4.Controllers
 {
+
     public class HomeController : Controller
     {
         // GET: Home
@@ -16,10 +22,58 @@ namespace SakerhetProjektGrupp4.Controllers
         [HttpPost]
         public ActionResult Index(string anvNamn, string losord)
         {
-            var a = anvNamn; 
-            var b = losord;
+            //TODO: Skapa metod för LoginCheck till REST
             return View();
         }
+
+        //Hosted web API REST Service base url  
+        string Baseurl = "http://localhost:54501/"; //Minnas
+        public async Task<ActionResult> RESTKoppling(string anvNamn, string losord)
+        {
+
+            List<AnvandarModel> AnvInfo = new List<AnvandarModel>();
+            try
+            {
+
+                using (var client = new HttpClient())
+                {
+                    //Passing service base url  
+                    client.BaseAddress = new Uri(Baseurl);
+
+                    client.DefaultRequestHeaders.Clear();
+                    //Define request data format  
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                    HttpResponseMessage Res = await client.GetAsync("Anvandares/1" );
+
+                    //Checking the response is successful or not which is sent using HttpClient  
+                    if (Res.IsSuccessStatusCode)
+                    {
+                        //Storing the response details recieved from web api   
+                        var AnvSvar = Res.Content.ReadAsStringAsync().Result;
+
+                        //Deserializing the response recieved from web api and storing into the Employee list  
+                        AnvInfo = JsonConvert.DeserializeObject<List<AnvandarModel>>(AnvSvar);
+
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Något gick fel" + e.Message);
+                throw;
+            }
+            finally
+            {
+                //close connection
+            }
+            //returning the employee list to view  
+            return View(EmpInfo);
+        }
+  
+
         
 
     }
---
+++ SakerhetProjektGrupp4/Models/AnvandarModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SakerhetProjektGrupp4.Models
+{
+    public class AnvandarModel
+    {
+        public int Id { get; set; }
+
+        public int AnvandarId { get; set; }
+
+     
+        public string Email { get; set; }
+
+   
+        public string Losenord { get; set; }
+    }
+}
