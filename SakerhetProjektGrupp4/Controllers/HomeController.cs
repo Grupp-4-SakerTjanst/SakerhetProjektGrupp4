612c478b88dd0469134fca93c21f3ff7fba3d543
Ganim-Bicoli <60735102+Ganim-Bicoli@users.noreply.github.com>
2020-06-02 15:24:44 +0200

    push -drift helt klar - authorization, igen klart.

--- SakerhetProjektGrupp4/Controllers/HomeController.cs
+++ SakerhetProjektGrupp4/Controllers/HomeController.cs
@@ -12,79 +12,86 @@ using System.Threading.Tasks;
 
 namespace SakerhetProjektGrupp4.Controllers
 {
-    
+
     public class HomeController : Controller
     {
         // GET: Home
-        
+
         public ActionResult Index()
         {
+            
             return View();
+       
         }
-
-        [AuthorizeUserAcessLevel(UserRole = 3)]
+        
         [HttpPost]
         public ActionResult Index(string anvNamn, string losord)
         {
-            return RedirectToAction("Index", "Personal");
+            
 
-            //PersonalModel PersMod = new PersonalModel {AnvandarNamn = anvNamn, Losenord = losord };
-            //if (anvNamn == null || losord == null)
-            //{
-            //    ModelState.AddModelError("", "Du måste fylla i både användarnamn och lösenord");
-            //    return View();
-            //}
+            PersonalModel PersMod = new PersonalModel { AnvandarNamn = anvNamn, Losenord = losord };
+            if (anvNamn == null || losord == null || anvNamn == "" || losord == "")
+            {
+                ModelState.AddModelError("index", "Du måste fylla i både användarnamn och lösenord");
+                return View();
+            }
+           
+            
+            bool validUser = false;
 
-      
-            //bool validUser = false;
+           
+            validUser = AnvCheck(PersMod);
 
-            ////Kontrollera mot webbservice
-            //validUser = AnvCheck(PersMod);
+            if (validUser == true)
+            {
+                System.Web.Security.FormsAuthentication.RedirectFromLoginPage(PersMod.AnvandarNamn, false);
+                return RedirectToAction("Index", "Personal");
+            }
+            ModelState.AddModelError("", "Inloggningen ej godkänd");
+            
+            return RedirectToAction("Index");
 
-            //if (validUser == true)
-            //{
-            //    System.Web.Security.FormsAuthentication.RedirectFromLoginPage(PersMod.AnvandarNamn, false);
-            //    return RedirectToAction("Index", "Personal");
-            //}
-            //ModelState.AddModelError("", "Inloggningen ej godkänd");
-            //return View();
 
         }
-        //private bool AnvCheck(PersonalModel Person)
-        //{
-        //    using (var client = new HttpClient())
-        //    {
 
-        //        client.BaseAddress = new Uri("http://193.10.202.74/inlogg/personals");
+       
+        private bool AnvCheck(PersonalModel Person)
+        {
+            using (var client = new HttpClient())
+            {
 
-        //        var response = client.PostAsJsonAsync("Login", Person).Result;
-        //        if (response.IsSuccessStatusCode)
-        //        {
-        //            string PersonRes = response.Content.ReadAsStringAsync().Result;
-        //            PersonalModel PersBehorig = JsonConvert.DeserializeObject<PersonalModel>(PersonRes);
-        //            if (PersBehorig != null)
-        //            {
-        //                if (PersBehorig.BehorighetsNiva == 3)
-        //                {
-        //                    return true;
-        //                }
-        //                else
-        //                {
-        //                    return false;
-        //                }    
-        //            }
-        //            else
-        //            {
-        //                return false;
-        //            }
-                     
-        //        }
-        //        else
-        //        {
-        //            return false;
-        //        }
-                 
-        //    }
-        //}
+                client.BaseAddress = new Uri("http://193.10.202.74/inlogg/personals");
+
+                var response = client.PostAsJsonAsync("Login", Person).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string PersonRes = response.Content.ReadAsStringAsync().Result;
+                    PersonalModel PersBehorig = JsonConvert.DeserializeObject<PersonalModel>(PersonRes);
+                    if (PersBehorig != null)
+                    {
+                        if (PersBehorig.BehorighetsNiva == 3)
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+        }
+
     }
+   
 }
